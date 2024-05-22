bb5e1e2c4b572b6552d5d1f28825bdb2b81cdd96
kfchen <kechanf@gmail.com>
2024-05-22 14:52:13 +0800

    调试

--- nnUNet/nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnUNet/nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -713,19 +713,6 @@ class nnUNetTrainer(object):
             patch_size_spatial = patch_size
             ignore_axes = None
 
-        tr_transforms.append(SpatialTransform(
-            patch_size_spatial, patch_center_dist_from_border=None,
-            do_elastic_deform=False, alpha=(0, 0), sigma=(0, 0),
-            do_rotation=True, angle_x=rotation_for_DA['x'], angle_y=rotation_for_DA['y'], angle_z=rotation_for_DA['z'],
-            p_rot_per_axis=1,  # todo experiment with this
-            do_scale=True, scale=(0.7, 1.4),
-            border_mode_data="constant", border_cval_data=0, order_data=order_resampling_data,
-            border_mode_seg="constant", border_cval_seg=border_val_seg, order_seg=order_resampling_seg,
-            random_crop=False,  # random cropping is part of our dataloaders
-            p_el_per_sample=0, p_scale_per_sample=0.2, p_rot_per_sample=0.2,
-            independent_scale_for_each_axis=False  # todo experiment with this
-        ))
-
         if do_dummy_2d_data_aug:
             tr_transforms.append(Convert2DTo3DTransform())
 
